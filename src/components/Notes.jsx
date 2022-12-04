3ab8f943dbf3203d0cc12646470dac288400f57d
Alen Jose <75483634+alen4152@users.noreply.github.com>
2022-12-05 00:13:53 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Deep Learning</h1>
+      <p>
+        Deep learning is a subset of machine learning, which is essentially a
+        neural network with three or more layers. These neural networks attempt
+        to simulate the behavior of the human brain—albeit far from matching its
+        ability—allowing it to “learn” from large amounts of data.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
